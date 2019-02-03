3b70f48597c267fe8e8561d6685008db06d09ffc
Raf <rafiou.diallo24@gmail.com>
2019-02-03 15:39:23 +0100

    mise à jour model

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -18,7 +18,6 @@ public class Product {
     @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
 
-    @Min(value = 1, message="Le prix de vente doit être supérieur à 0")
     private int prix;
 
     //information que nous ne souhaitons pas exposer
